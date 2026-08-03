1daa54fed5b563bfb15fdf064f01d497db6251d9
tunggalpaito <tunggalpaito@gmail.com>
2026-08-04 01:50:46 +0700

    Add files via upload

---
+++ data/macau.js
@@ -0,0 +1,38 @@
+const DATA_MACAU = {
+    "senin": {
+        "p1": "89 45 23 - BB CAMPURAN",
+        "p2": "AK47 JITU DAN MACAN",
+        "p3": "77 88 ANGKA KEMBAR",
+        "p4": "90 12 AMAN",
+        "p5": "BOCORAN MALAM 43",
+        "p6": "00 11 FORMAT BEBAS"
+    },
+    "selasa": {
+        "p1": "ANGKA SELASA 11 22",
+        "p2": "34 56 JOSS",
+        "p3": "78 90 BOM",
+        "p4": "WASPADA ANGKA KECIL",
+        "p5": "55 66 77",
+        "p6": ""
+    },
+    "rabu": {
+        "p1": "",
+        "p2": "ANGKA MAIN 12",
+        "p3": "ANGKA MAIN 19",
+        "p4": "ANGKA MAIN 96",
+        "p5": "",
+        "p6": ""
+    },
+    "kamis":  { 
+        "p1":"", 
+        "p2":"", 
+        "p3":"", 
+        "p4":"", 
+        "p5":"", 
+        "p6":"" 
+    },
+    
+    "jumat":  { "p1":"", "p2":"", "p3":"", "p4":"", "p5":"", "p6":"" },
+    "sabtu":  { "p1":"", "p2":"", "p3":"", "p4":"", "p5":"", "p6":"" },
+    "minggu": { "p1":"", "p2":"", "p3":"", "p4":"", "p5":"", "p6":"" }
+    };
