f2007fe2b7c8c69df39ef7e8c801a0a78de85ad6
tunggalpaito <tunggalpaito@gmail.com>
2026-08-17 16:07:54 +0700

    Update macau.js

--- result/macau.js
+++ result/macau.js
@@ -57,9 +57,9 @@ const DATA_PAITO_MACAU = `
   14 Aug,0143,3920,3881,4220,1828,5386,
   15 Aug,6344,9494,8480,6464,3062,6021,
   16 Aug,1126,1480,2750,7204,7003,2060,
-  17 Aug,8474,1143,
+  17 Aug,8474,1143,4641
 `;
 
 const LIVE_RESULT_MACAU = {
-    latestResult: "1143"
+    latestResult: "4641"
 };
